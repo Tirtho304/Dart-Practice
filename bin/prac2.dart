413462c59864334fac80dcaf3846525bae924b5a
Tirtho304 <shirdartho15-13304@diu.edu.bd>
2022-05-09 17:11:19 +0600

    commit3

--- bin/prac2.dart
+++ bin/prac2.dart
@@ -5,4 +5,27 @@ void main(List<String> args) {
   } else {
     print("This is even");
   }
+
+  //else if
+  var value2 = 45000;
+  if (value2 < 4000) {
+    print("The is  good salaray");
+  } else if (value2 != 45000) {
+    print("You have get no salary");
+  } else {
+    print("NO salary");
+  }
+
+  //ternary oparator
+
+  var num = 3;
+  var num2 = 4;
+  num < num2 ? print("$num is smaller") : print("$num2 is smaller");
+
+  //?? oparator
+  // jdi name variable ta faka thake tbe ?? er pore jeta print krete bola hoise oita print hbbe
+
+  var name = null;
+  var a = name ?? "No name";
+  print(a);
 }
