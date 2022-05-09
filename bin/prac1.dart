9e70058600cd4e017af849f61bff4f2a33564cb1
Tirtho304 <shirdartho15-13304@diu.edu.bd>
2022-05-09 16:50:50 +0600

    change2

---
+++ bin/prac1.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  double a = 70;
+  print(a);
+  //int hexavalue =
+  var name = "Tirtho";
+  var name2 = 'It\'s tirtho';
+  print("$name");
+  print("$name2");
+  print(name + " " + name2);
+}
---
+++ bin/prac2.dart
@@ -0,0 +1,8 @@
+void main(List<String> args) {
+  int value = 20;
+  if (value % 2 == 0) {
+    print("This is odd number");
+  } else {
+    print("This is even");
+  }
+}
